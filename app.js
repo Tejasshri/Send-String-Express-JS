96c4850c050bc263efae4bdd46b755a58c08f635
tejasshri <tejasshrishekhar08@gmail.com>
2023-07-31 05:06:12 +0000

    change String

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("express js");
+  response.send("Express JS");
 });
 
 app.listen(3000);
